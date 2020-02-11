db711fba36210ee98b8394a947773262dbf29ab2
sh4mi <sh4mi@hotmail.com>
2020-02-11 12:53:47 +0500

    pushing to dev

--- application/views/login.php
+++ application/views/login.php
@@ -20,7 +20,7 @@
   <body class="hold-transition login-page">
     <div class="login-box">
       <div class="login-logo">
-        <a href="#"><b>CodeInsect</b><br>Admin System</a>
+        <a href="#"><b>Spark Databox</b><br>Admin System</a>
       </div><!-- /.login-logo -->
       <div class="login-box-body">
         <p class="login-box-msg">Sign In</p>
